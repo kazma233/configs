6c6f2c170023086cf45ad67cfb95fea3745a9c4d
kazma233 <kazma233@outlook.com>
2026-02-05 16:50:34 +0800

    feat: split config

--- WezTerm/wezterm-mac.lua
+++ WezTerm/wezterm-mac.lua
@@ -9,6 +9,10 @@ local config = wezterm.config_builder()
 config.initial_cols = 120
 config.initial_rows = 20
 
+config.font = wezterm.font 'Fira Code'
+config.font = wezterm.font_with_fallback {
+  'JetBrains Mono',
+}
 config.font_size = 18
 config.color_scheme = 'Vs Code Light+ (Gogh)'
 
